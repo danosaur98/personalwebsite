9b655fd313669a62f0b2c9738b75989b881f6ccf
Daniel Zhou <daniel.zhou2012@gmail.com>
2018-11-29 16:00:59 -0500

    added git

--- src/Lightning.js
+++ src/Lightning.js
@@ -14,29 +14,19 @@ export default function Lightning(x, y, length, angle, ctx) {
 
     console.log(this.x, this.y)
     this.draw = function (length) {
-        this.paths.forEach(function (p){
-            p.ctx.save();
-            p.ctx.beginPath();
-            p.ctx.translate(p.x,p.y);
-            p.ctx.moveTo(0, 0);
-            p.ctx.rotate(p.angle);
-            p.ctx.lineTo(0, length);
-            p.ctx.strokeStyle = 'yellow';
-            p.ctx.stroke();
-            p.ctx.restore();
-        })
+        // this.ctx.save();
+        // this.ctx.beginPath();
+        // this.ctx.translate(this.x, this.y);
+        // this.ctx.moveTo(0, 0);
+        // this.ctx.rotate(this.angle);
+        // this.ctx.lineTo(0, length);
+        // this.ctx.strokeStyle = 'yellow';
+        // this.ctx.stroke();
+        // this.ctx.restore();
         
     }
 
-    this.update = function () {
-        var lastPath = this.paths[this.paths.length - 1];
-        this.paths.push({
-            x: lastPath.x + Math.cos(lastPath.angle)*(lastPath.length+1),
-            y: lastPath.y - Math.sin(lastPath.angle)*(lastPath.length+1),
-            length: lastPath.length + 1,
-            angle: lastPath.angle + Math.PI/8,
-            ctx: this.ctx
-        })
+    this.uthisdate = function () {
         this.draw(this.length);
     }
 }
